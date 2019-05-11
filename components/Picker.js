7657f2825b1fac30adbef445c2615113a486c09a
user154046 <user154046@AS419.local>
2019-05-11 10:54:36 +0800

    Changed pickers to modal selectors for better compatibility with iOS

--- components/Picker.js
+++ components/Picker.js
@@ -4,30 +4,39 @@ import { changeCountry, calculateTip } from "../actions";
 import { countries } from "../countryList";
 import { Picker, Alert, View, Text } from "react-native";
 import styles from "../styles/pickerStyles";
+import ModalSelector from 'react-native-modal-selector'
 
 class CountryPicker extends Component {
   handleChange = event => {
     this.props.changeCountry(event);
   };
   render() {
-    const pickerItems = countries.map(country => {
-      return (
-        <Picker.Item
-          key={country.name}
-          label={country.name}
-          value={country.name}
-        />
-      );
+    // const pickerItems = countries.map(country => {
+      // return (
+        // <Picker.Item
+        //   key={country.name}
+        //   label={country.name}
+        //   value={country.name}
+        // />
+        
+    //   );
+    // });
+    const pickerItems = countries.map((val, i) => {
+      return {key: i, label: String(val.name)}
     });
     return (
-      <Picker
-        selectedValue={this.props.country.name}
-        style={styles.picker}
-        textStyle={{ fontSize: 40 }}
-        onValueChange={value => this.handleChange(value)}
-      >
-        {pickerItems}
-      </Picker>
+      // <Picker
+      //   selectedValue={this.props.country.name}
+      //   style={styles.picker}
+      //   textStyle={{ fontSize: 40 }}
+      //   onValueChange={value => this.handleChange(value)}
+      // >
+      //   {pickerItems}
+      // </Picker>
+      <ModalSelector
+                  data={pickerItems}
+                  initValue={this.props.country.name}
+                  onChange={value => this.handleChange(value.label)}/>
     );
   }
 }
